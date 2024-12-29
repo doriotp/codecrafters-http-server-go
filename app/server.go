89ed8b90f8938701e69abbfebd6400d1258d0e85
Amitkumar631 <amitkhan1911@gmail.com>
2024-12-29 22:28:09 +0530

    third task;extract url path

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 )
 
@@ -16,17 +17,25 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
+	// l, err := net.Listen("tcp", "0.0.0.0:4221")
+	// if err != nil {
+	// 	fmt.Println("Failed to bind to port 4221")
+	// 	os.Exit(1)
+	// }
 	
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	// conn, err := l.Accept()
+	// if err != nil {
+	// 	fmt.Println("Error accepting connection: ", err.Error())
+	// 	os.Exit(1)
+	// }
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	http.HandleFunc("/abcdefg", func(rw http.ResponseWriter, r *http.Request){
+		rw.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	})
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request){
+		rw.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	})
+
+	http.ListenAndServe(":4221", nil)
 }
